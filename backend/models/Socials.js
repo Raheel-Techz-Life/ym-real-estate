543baede734a998a3da2f793b8f43c55668ba89b
Raheel Hosmani <raheelhosmani05@gmail.com>
2025-12-09 19:42:00 +0530

    Update Socials.js

--- backend/models/Socials.js
+++ backend/models/Socials.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const SocialsSchema = new mongoose.Schema({
-  identifier: { type: String, default: 'main_socials', unique: true }, // Singleton pattern
+  identifier: { type: String, default: 'main_socials', unique: true },
   instagram: { type: Array, default: [] },
   youtube: { type: Array, default: [] },
   facebook: { type: Array, default: [] }
